b31b32d8d3ce204af27bddaa20e1875cc1cd4e13
Anthony Haslett <Anthony-Haslett@outlook.com>
2019-04-01 22:21:48 +0100

    genesis(IndexPage): first commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
-
+import React, { Component } from "react";
+import "./app.css";
+// import ReactImage from './react.png';
 export default class App extends Component {
-  state = { username: null };
-
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    fetch("/api/getUsername").then(res => res.json());
   }
 
   render() {
-    const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <h1>Club one - making your night out work for you!</h1>
+        <nav>
+          <a href="/html/">HTML</a> 
+          <a href="/css/">CSS</a>
+          <a href="/js/">JavaScript</a>
+          <a href="/jquery/">jQuery</a>
+        </nav>
       </div>
     );
   }
