9a9778f4b6dff13dcc17734fd857cebad74f65ea
Anthony Haslett <Anthony-Haslett@outlook.com>
2019-04-21 22:08:04 +0100

    maintenance(Setup): adding core requirements

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (module.hot) {
+  module.hot.accept('./App.js', function () {
+    console.log("hot")
+  })
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,62 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
-const outputDirectory = 'dist';
+const dist = 'dist';
 
 module.exports = {
   entry: ['babel-polyfill', './src/client/index.js'],
   output: {
-    path: path.join(__dirname, outputDirectory),
+    path: path.join(__dirname, dist),
     filename: 'bundle.js'
   },
-  module: {
-    rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
+  devtool: 'inline-source-map',
+
   devServer: {
+    contentBase: './dist',
+    hot: true,
     port: 3000,
     open: true,
     proxy: {
       '/api': 'http://localhost:8080'
     }
   },
+
   plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
+    new CleanWebpackPlugin([dist]),
     new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
       template: './public/index.html',
       favicon: './public/favicon.ico'
-    })
-  ]
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
 };
